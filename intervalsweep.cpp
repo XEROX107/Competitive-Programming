69f59c2eba5bed76dd1950258303a063bb7e3655
SakiB AhmeD <149652752+XEROX107@users.noreply.github.com>
2025-10-22 23:42:18 +0600

    Add files via upload

---
+++ intervalsweep.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if ((a + b) > 0 && abs(a - b) <= 1)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
